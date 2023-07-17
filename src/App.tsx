31e9d719f2091d75725aa73a71d1c7bdcd5c5fa1
diegomartignoni <m.s.d.d.diego@gmail.com>
2023-07-17 13:57:26 +0200

    backlog-web-fix: add fontawesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHandHoldingHeart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 function App() {
+  library.add(faHandHoldingHeart)
   return (
-    <h1>Hello</h1>
+    <>
+      <h1>Hello</h1>
+      <FontAwesomeIcon icon={["fas", "hand-holding-heart"]} />
+    </>
   );
 }
 
